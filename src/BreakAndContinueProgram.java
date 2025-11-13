e96d7c51f4e54f6a9287c01d6e8e566b4c175d42
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-11-13 19:25:45 +0530

    File created for issue : 1

---
+++ src/BreakAndContinueProgram.java
@@ -0,0 +1,6 @@
+public class BreakAndContinueProgram {
+    public static void main(String[] args){
+        /* creating a waiter asking for order , if it is "biriyani" , then it is skipping that order */
+
+    }
+}
