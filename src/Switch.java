88c6fa7efa02919987c97b1192e32b68dcb57a29
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-11-13 18:51:27 +0530

    Switch case program

---
+++ src/Switch.java
@@ -0,0 +1,31 @@
+public class Switch {
+    public static void main(String[] args){
+        int day = 83;
+
+        switch(day){
+            case 1:
+                System.out.println("It's sunday..");
+            case 2:
+                System.out.println("Monday!!!");
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("WednesDay");
+                break;
+            case 5:
+                System.out.println("Thursday.");
+                break;
+            case 6:
+                System.out.println("FriDay..");
+                break;
+            case 7:
+                System.out.println("Saturday..");
+                break;
+
+                default:
+                    System.out.println(11 + 2);
+
+        }
+    }
+}
