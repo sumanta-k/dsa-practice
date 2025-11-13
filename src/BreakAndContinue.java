f847ce8627603af03f41ca188a517367b717ab93
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-11-13 19:03:27 +0530

    Whenever break statement hitting , no need to execute further , that's why break is used

--- src/BreakAndContinue.java
+++ src/BreakAndContinue.java
@@ -1,5 +1,11 @@
 public class BreakAndContinue {
     public static void main(String[] args){
-
+    /* how to use break statement  and what it does */
+        for(int i = 0; i < 10; i++){
+            if (i == 4){
+                break; /* whenever break statement hitting , it is telling that stop this execution , no need to further go */
+            }
+            System.out.println(i);
+        }
     }
 }
